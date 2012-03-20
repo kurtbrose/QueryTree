882b231b0703c5688f2e1ff0e08aaea2446890c4
Kurt Rose <kurose@paypal.com>
2012-03-20 12:44:09 -0700

    adding more jQuery functions and predicates

--- qtree.py
+++ qtree.py
@@ -15,15 +15,58 @@ append, appendTo... not sure what to do with these
 '''
 import collections
 
+def build(data, get_children):
+    '''
+    Main entry point; when passed a root piece of data and a function that
+    knows how to fetch the child data, will build up a Query-able tree of Nodes
+    attached to that data.
+    '''
+    root = Node(None, [], data)
+    todo = [root]
+    while len(todo) > 0:
+        cur = todo.pop()
+        for d in get_children(cur.data):
+            cur.child_nodes.append(Node(cur, [], d))
+        todo.extend(cur.child_nodes)
+    return root
+
+def _node_walk(node, predicate):
+    todo = [node]
+    matches = Matches()
+    while len(todo) > 0:
+        node = todo.pop()
+        if predicate(node.data):
+            matches.items.append(node)
+        todo.extend(node.children)
+    return matches
+
 class Node(object):
-    __slots__ = ("parent", "children", "data")
+    __slots__ = ("parent", "child_nodes", "data")
     
-    def __init__(self, parent, children, data):
+    def __init__(self, parent, child_nodes, data):
         self.parent = parent
-        self.children = children
+        self.child_nodes = child_nodes
         self.data = data
-        
-    def appendTo(self, ): pass
+    
+    def attr(self, k): return getattr(data, k, None)
+    
+    def children(self, predicate):
+        return [c for c in self.child_nodes if predicate(c)]
+    
+    def closest(self, predicate):
+        'Searches up the tree for the first parent which matches'
+        cur = self
+        while not predicate(cur):
+            cur = cur.parent
+            if cur is None:
+                break
+        return cur
+    
+    def find(self, predicate):
+        return _node_walk(self, predicate)
+    
+    def __repr__(self):
+        return "Node(data="+repr(self.data)+")"
 
 class Matches(collections.MutableSequence):
     __slots__ = ("items",)
@@ -50,12 +93,50 @@ class Matches(collections.MutableSequence):
     def after(self, data):
         for i in self.items:
             for p in i.parents:
-                p.children.insert(p.index(i)+1, Node(p, [], data))
+                p.child_nodes.insert(p.index(i)+1, Node(p, [], data))
         return self #todo: should this modify the current Match set?
     
     def append(self, data):
         for i in self.items:
-            i.children.append(Node(i.parent, [], data))
+            i.child_nodes.append(Node(i.parent, [], data))
         return self #todo: return a different Match set?
     
+    def attr(self, k):
+        return [i.attr(k) for i in self.items]
+    
+    def children(self, predicate=None):
+        if predicate is None:
+            return Matches(sum([i.child_nodes for i in self.items], []))
+        return Matches(sum([i.children(predicate) for i in self.items], []))
+    
+    def data(self):
+        '''
+        After the query is "done", re-extract the data items.
+        '''
+        return [i.data for i in self.items]
+    
+    def each(self, f):
+        return [f(i.data) for i in self.items]
+    
+    def empty(self):
+        for i in self.items:
+            i.child_nodes = []
+    
+    def find(self, predicate):
+        return Matches(sum([i.find(predicate).items for i in self.items], []))
+    
+    def filter(self, predicate):
+        return Matches([i for i in self.items if predicate(i.data)])
+    
+    def __repr__(self):
+        return "Matches("+repr(self.items)+")"
+    
+####a bunch of predicates
+
+def empty(node):
+    'predicate for selecting nodes with no children'
+    return len(node.child_nodes) == 0
+
+def even(node):
+    'predicate for selecting the even numbered elements'
     
